4dc49508de8b697cf590920125a9507427b5da97
Hazel Wu <tinazx056@gmail.com>
2017-10-16 20:10:44 +0800

    Decoration for style.css & Added require form ftn & photo.created at time

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
   before_action :set_photo, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @photos = Photo.all
+    @photos = Photo.all.order("created_at DESC")
   end
 
   def new
